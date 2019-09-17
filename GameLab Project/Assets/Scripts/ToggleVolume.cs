b4745b868387a111230879509ec25faa168a25f5
NikoPikka <38316171+NikoPikka@users.noreply.github.com>
2019-09-17 12:53:48 +0300

    Menu scripts

---
+++ GameLab Project/Assets/Scripts/ToggleVolume.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleVolume : MonoBehaviour
+{
+    private AudioManager audioManager;
+    public Button soundToggleButton;
+    public Sprite soundOn;
+    public Sprite soundOff;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioManager = GameObject.FindObjectOfType<AudioManager>();
+        UpdateIconAndVolume();
+    }
+
+    public void PauseVolume()
+    {
+        audioManager.ToggleVolume();
+        UpdateIconAndVolume();
+    }
+
+    void UpdateIconAndVolume()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            soundToggleButton.GetComponent<Image>().sprite = soundOn;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            soundToggleButton.GetComponent<Image>().sprite = soundOff;
+        }
+    }
+}
