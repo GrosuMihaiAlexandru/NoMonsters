e5a365caa53fa8440349a08f03711fdb4296b600
NikoPikka <n7pini00@students.oamk.fi>
2019-09-18 13:04:36 +0300

    MainMenu + GameUI

--- GameLab Project/Assets/Scripts/UI/MainMenu.cs
+++ GameLab Project/Assets/Scripts/UI/MainMenu.cs
@@ -9,5 +9,8 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
