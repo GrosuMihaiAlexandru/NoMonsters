bfed86689aebc9c94fe0a0c87429b8e94bdc3838
Raul Brumar <raulbrumar@yahoo.de>
2019-10-16 10:36:44 +0300

    Worked on ads

--- Ice on the Line/Assets/Scripts/Monetization/AdsManagerScript.cs
+++ Ice on the Line/Assets/Scripts/Monetization/AdsManagerScript.cs
@@ -1,8 +1,14 @@
 ﻿using EasyMobile;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AdsManagerScript : MonoBehaviour
 {
+    private AdMobClientImpl adMobClient;
+
+    [SerializeField]
+    private List<RectTransform> thingsToMoveUpWhenBannerAdIsShowing = new List<RectTransform>();
+
     private void Awake()
     {
         if (!RuntimeManager.IsInitialized())
@@ -10,22 +16,26 @@ public class AdsManagerScript : MonoBehaviour
             RuntimeManager.Init();
             Advertising.GrantDataPrivacyConsent();
         }
+        adMobClient = Advertising.AdMobClient;
+        adMobClient.OnBannerAdOpening += AdMobClient_OnBannerAdOpening;
+        // adMobClient.OnBannerAdClosed += AdMobClient_OnBannerAdClosed;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShowBannerAd();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Advertising.ShowBannerAd(BannerAdPosition.Bottom);
-        if (Advertising.IsRewardedAdReady())
-        {
-            print("rewarded ad ready");
-        }
+        
+    }
+
+    private void OnDestroy()
+    {
+        Advertising.DestroyBannerAd();
     }
 
     public void ShowBannerAd()
@@ -33,11 +43,26 @@ public class AdsManagerScript : MonoBehaviour
         Advertising.ShowBannerAd(BannerAdPosition.Bottom);
     }
 
-    public void ShowRewardedAd()
+    private void AdMobClient_OnBannerAdOpening(object sender, System.EventArgs e)
     {
-        if (Advertising.IsRewardedAdReady())
+        for(int i = 0; i < thingsToMoveUpWhenBannerAdIsShowing.Count; i++)
         {
-            Advertising.ShowRewardedAd();
+            thingsToMoveUpWhenBannerAdIsShowing[i].anchoredPosition += new Vector2(0, GetBannerHeight());
         }
+    }
+
+    /*
+    private void AdMobClient_OnBannerAdClosed(object sender, System.EventArgs e)
+    {
+        for (int i = 0; i < thingsToMoveUpWhenBannerAdIsShowing.Count; i++)
+        {
+            thingsToMoveUpWhenBannerAdIsShowing[i].anchoredPosition -= new Vector2(0, GetBannerHeight());
+        }
+    }
+    */
+
+    public float GetBannerHeight()
+    {
+        return Mathf.RoundToInt(50 * Screen.dpi / 160);
     }
 }
