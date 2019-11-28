5b1cd5cce23b1cd1aff570f8b6cb6ccb2e249a07
Raul Brumar <raulbrumar@yahoo.de>
2019-11-28 19:50:11 +0200

    Added Unity Analytics custom triggers for campaigns
    plus some extra code for notifications

---
+++ Ice on the Line/Assets/Scripts/NotificationsHandler.cs
@@ -0,0 +1,88 @@
+﻿using EasyMobile;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationsHandler : MonoBehaviour
+{
+    public NotificationsHandler instance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        if (!RuntimeManager.IsInitialized())
+            RuntimeManager.Init();
+
+        Notifications.GrantDataPrivacyConsent();
+
+        int areNotificationsSet = PlayerPrefs.GetInt("RepeatigNotificationsSet", 0);
+        if (areNotificationsSet == 0)
+        {
+            Invoke("ScheduleRepeatingLocalNotificationAfterDelay", 2f);
+            PlayerPrefs.SetInt("RepeatigNotificationsSet", 1);
+        }
+    }
+
+    void ScheduleRepeatingLocalNotificationAfterDelay(string title, string subtitle, string body)
+    {
+        // Prepare the notification content (see the above section).
+        NotificationContent content = PrepareNotificationContent("PLAY NOW!", "The penguin misses you", "You haven't played in a while. Have another go!");
+
+        // Set the delay time as a TimeSpan.
+        TimeSpan delay = new TimeSpan(0, 1, 0);
+
+        // Schedule the notification.
+        // Notifications.ScheduleLocalNotification(delay, content, NotificationRepeat.EveryMinute);
+        Notifications.ScheduleLocalNotification(delay, content, NotificationRepeat.EveryMinute);
+    }
+
+    NotificationContent PrepareNotificationContent(string title, string subtitle, string body)
+    {
+        NotificationContent content = new NotificationContent();
+
+        // Provide the notification title.
+        content.title = "Demo Notification";
+
+        // You can optionally provide the notification subtitle, which is visible on iOS only.
+        content.subtitle = "Demo Subtitle";
+
+        // Provide the notification message.
+        content.body = "This is a demo notification.";
+
+        // You can optionally attach custom user information to the notification
+        // in form of a key-value dictionary.
+        // content.userInfo = new Dictionary<string, object>();
+        // content.userInfo.Add("string", "OK");
+        // content.userInfo.Add("number", 3);
+        // content.userInfo.Add("bool", true);
+
+        // You can optionally assign this notification to a category using the category ID.
+        // If you don't specify any category, the default one will be used.
+        // Note that it's recommended to use the category ID constants from the EM_NotificationsConstants class
+        // if it has been generated before. In this example, UserCategory_notification_category_test is the
+        // generated constant of the category ID "notification.category.test".
+
+        // uncomment to set a category
+        // content.categoryId = EM_NotificationsConstants.UserCategory_notification_category_Ice;
+
+        // If you want to use default small icon and large icon (on Android),
+        // don't set the smallIcon and largeIcon fields of the content.
+        // If you want to use custom icons instead, simply specify their names here (without file extensions).
+
+        // content.smallIcon = "ic_stat_cooltemperature";
+        // content.largeIcon = "ic_stat_cooltemperature";
+
+        return content;
+    }
+}
