ec48a27e71c0fccad01ef3797e77256926345676
Alexandru Grosu <alex.gm98@yahoo.com>
2019-12-02 13:59:00 +0200

    Added Powerup Display

---
+++ Ice on the Line/Assets/Scripts/Powerups/ActivePowerupDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivePowerupDisplay : MonoBehaviour
+{
+    public bool powerupActive = false;
+
+    private Image powerupImage;
+
+    public List<Sprite> powerupSprites;
+
+    public Image timerCircle;
+
+    private void Awake()
+    {
+        powerupImage = GetComponentInChildren<Image>();
+        StartCoroutine(PowerupTickUpdate(20f));
+    }
+
+    
+    private IEnumerator PowerupTickUpdate(float time)
+    {
+        for(float i = 0; i < time; i += 0.01f)
+        {
+            timerCircle.fillAmount = ReMap(i, 0, 3, 0, 1);
+            yield return new WaitForSeconds(.01f);
+        }
+    }
+
+    private float ReMap(float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
+}
